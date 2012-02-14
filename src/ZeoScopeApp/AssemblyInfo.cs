d43cbda01d977b9225d7ebdee8162e54f4d11893
Dan <dancodru@gmail.com>
2012-02-13 23:45:58 -0800

    Increased version to 0.9.5.1

--- src/ZeoScopeApp/AssemblyInfo.cs
+++ src/ZeoScopeApp/AssemblyInfo.cs
@@ -20,5 +20,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("0.9.5.0")]
-[assembly: AssemblyFileVersion("0.9.5.0")]
+[assembly: AssemblyVersion("0.9.5.1")]
+[assembly: AssemblyFileVersion("0.9.5.1")]
